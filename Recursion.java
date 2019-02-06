ee21b6cc38cf2175b70ac8d662b7d9925ab99c62
Kayla Fang <kfang00@stuy.edu>
2019-02-06 00:35:07 -0500

    added test cases and fixed some compile errors

--- Recursion.java
+++ Recursion.java
@@ -6,9 +6,9 @@ public class Recursion {
     return mAWH(k, maxLetter, "", hold);
   }
 
-    private static List<String> mAWH(int k, int max, String word, List<String> data) {
-      if (k = 0) {
-        data.add(word)
+    private static ArrayList<String> mAWH(int k, int max, String word, ArrayList<String> data) {
+      if (k == 0) {
+        data.add(word);
       }
       if (k > 0) {
         for (int n = 0; n < max; n++) {
@@ -16,5 +16,14 @@ public class Recursion {
         }
       }
     return data;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(makeAllWords(1, 1)); 
+    System.out.println(makeAllWords(1, 2));
+    System.out.println(makeAllWords(1, 3));
+    System.out.println(makeAllWords(1, 4));
+    System.out.println(makeAllWords(1, 5));
+
   }
 }
